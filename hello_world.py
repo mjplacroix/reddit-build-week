1b8968f844b9bcfbceb9bc7511751dfcc292ff48
Michael LaCroix <michael.jp.lacroix@gmail.com>
2020-03-02 14:26:41 -0500

    adding hello-world.py file to test basic functionality of flask and heroku

---
+++ hello_world.py
@@ -0,0 +1,21 @@
+"""test page for Flask and Heroku"""
+
+
+from flask import Flask, render_template
+
+# make the application
+app = Flask(__name__)
+
+# make the route
+@app.route("/")
+
+#def a function
+def hello():
+    return "Hello World!!"
+
+# # make second route
+# @app.route("/about")
+
+# # func for about
+# def preds():
+#     return render_template('about.html')
